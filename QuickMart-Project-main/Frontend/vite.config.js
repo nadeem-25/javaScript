a3c15cce11e3ec462413bad9697394df5012136c
Nadeem Ahmad <nadeem25official@gmail.com>
2026-09-07 14:36:30 +0530

    Add rollupOptions for multiple HTML entry points

--- QuickMart-Project-main/Frontend/vite.config.js
+++ QuickMart-Project-main/Frontend/vite.config.js
@@ -1,4 +1,5 @@
 import {defineConfig} from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
     root:".",
@@ -8,6 +9,13 @@ export default defineConfig({
     },
 
     build:{
-        outDir:"dist"
+        outDir:"dist",
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                home: resolve(__dirname, "src/Home/home.html"),
+                cart: resolve(__dirname, "src/Cart/cart.html"),
+            }
+        }
     }
 })
